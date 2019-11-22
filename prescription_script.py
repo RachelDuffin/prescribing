05dd1872e7d96e8ec6aeba372a3dd4653f478c25
Rachel Duffin <rachel.g.duffin@gmail.com>
2019-11-22 00:21:13 +0000

    defined function for downloading data from APIs

--- prescription_script.py
+++ prescription_script.py
@@ -1,30 +1,32 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
-#pip3 install requests numpy pandas 
+#pip3 install requests numpy pandas matplotlib
 
-#Import required packages----------------------------------------------------------------------------------------------------------------------------------------------------------------
+#Import required packages-------------------------------------------------------------------------------------------------------------------------------------------------------
 import requests, csv, json, sys
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-#Download the data using APIs and load into pandas dataframe------------------------------------------------------------------------------------------------------------------------------------------------------------
+#MANCHESTER CCG-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+#Define functions
 
-spending = requests.get('https://openprescribing.net/api/1.0/spending_by_practice/?code=5.1&format=json&org=14L').json()#gets json file of antibiotics prescribed per practice in Manchester CCG in json format
-print(json.dumps(spending, indent=4, sort_keys=True)) #print data
-spendingdf = pd.DataFrame.from_dict(spending) #Creates pandas dataframe from the dictionary format of a json file
-print(spendingdf) #prints the spending dataframe
+#Download data in json format and load into pandas dataframe
+def download(API):
+    df = pd.DataFrame.from_dict(requests.get(API).json()) #gets data from API in json format, and creates pandas dataframe from dictionary format of json file 
+    return(df)
 
-list_size = requests.get('https://openprescribing.net/api/1.0/org_details/?format=json&org_type=practice&org=14L&keys=total_list_size').json() #gets total list size per GP practice in Manchester CCG in json format
-print(json.dumps(list_size, indent=4, sort_keys=True)) #print data
-listsizedf = pd.DataFrame.from_dict(list_size) #Creates pandas dataframe from the dictionary format of a json file
-print(listsizedf) #prints the listsize dataframe
+#Download the data using APIs and load into pandas dataframe--------------------------------------------------------------------------------------------------------------------
 
-#Merge the two pandas data frames--------------------------------------------------------------------------------------------------------------------------------------------------------
+spending = download(API='https://openprescribing.net/api/1.0/spending_by_practice/?code=5.1&format=json&org=14L') #gets json file of antibiotics prescribed per practice in Manchester CCG in json format
+print(spending) #prints output of function to test output is correct
 
-new_df = pd.merge(spendingdf, listsizedf, on=['row_id','date', 'row_name']) #merges the data frames on 'row_id' and 'date'
+listsize = download(API='https://openprescribing.net/api/1.0/org_details/?format=json&org_type=practice&org=14L&keys=total_list_size') #gets total list size per GP practice in Manchester CCG in json format
+print(listsize) #prints the listsize dataframe
 
-#Plot graph of prescribed items over time for each practice in Manchester CCG---------------------------------------------------------------------------------------------------------------------------------
+new_df = pd.merge(spendingdf, listsizedf, on=['row_id','date', 'row_name']) #merges the two pandas dataframes on 'row_id' and 'date'
+
+#Plot graph of prescribed items over time for each practice in Manchester CCG---------------------------------------------------------------------------------------------------
 
 #Basic graph 
 fig, ax = plt.subplots(figsize=(30,10)) #Plots graph of size 30,10
@@ -43,3 +45,30 @@ for key, grp in new_df.groupby(['row_name']):
 
 #Calculate the mean 
 prescribe_mean = new_df.groupby(by='date')['items per 1000'].mean() #Calculates mean prescriptions for each month per 1000 registered patients 
+
+
+#COMPARED TO ALL CCGs------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+#Download the data using APIs and load into pandas dataframe---------------------------------------------------------------------------------------------------------------------
+spending = requests.get('https://openprescribing.net/api/1.0/spending_by_ccg/?code=5.1&format=json').json()#gets json file of antibiotics prescribed per CCG
+#print(json.dumps(spending, indent=4, sort_keys=True)) #print data
+spendingdf = pd.DataFrame.from_dict(spending) #Creates pandas dataframe from the dictionary format of a json file
+#print(spendingdf) #prints the spending dataframe
+
+list_size = requests.get('https://openprescribing.net/api/1.0/org_details/?format=json&keys=total_list_size&org_type=ccg').json() #gets total list size per CCG
+print(json.dumps(list_size, indent=4, sort_keys=True)) #print data
+listsizedf = pd.DataFrame.from_dict(list_size) #Creates pandas dataframe from the dictionary format of a json file
+#print(listsizedf) #prints the listsize dataframe
+
+#Merge the two pandas data frames------------------------------------------------------------------------------------------------------------------------------------------------
+new_df = pd.merge(spendingdf, listsizedf, on=['row_id','date', 'row_name']) #merges the data frames on 'row_id' and 'date'
+
+#Calculate the mean antibiotics prescribed per 1000 patients across all CCGs-----------------------------------------------------------------------------------------------------
+
+new_df['items per 1000'] = new_df['items']/new_df['total_list_size']*1000 #Adds column to dataframe of prescribed items per 1,000 registered patients as some practices are smaller than others
+
+#Calculate the antibiotics prescribed per 1000 patients across Manchester CCG----------------------------------------------------------------------------------------------------
+prescribe_mean = new_df.groupby(by='date')['items per 1000'].mean() #Calculates mean prescriptions for each month per 1000 registered patients 
+
+#Plot graph of Manchester CCG----------------------------------------------------------------------------------------------------------------------------------------------------
+
