7ddfb88765b2e0c849541380178e0180b4744c94
Rachel Duffin <rachel.g.duffin@gmail.com>
2019-11-22 12:18:55 +0000

    Added graph headings and axis labels

--- prescription_script.py
+++ prescription_script.py
@@ -25,58 +25,43 @@ print(new_df) #prints to check df is correct and check normalised column has app
 #Plot graphs of prescribed items over time for each practice in Manchester CCG---------------------------------------------------------------------------------------------------
 
 #Define function for plotting graphs
-def plot(x, y):
+def plot(x, y, label, xlabel, ylabel):
     fig, ax = plt.subplots(figsize=(30,10)) #Plots graph of size 30,10
+    plt.title(label, loc='center', pad=None)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
     for key, grp in new_df.groupby(['row_name']): 
         ax = grp.plot(ax=ax, kind='line', x=x, y=y, label=key) #Specifies graph as line graph, and determines what is on each axis
     plt.show()
 
+
 #Plot graphs
-plot(x='date', y='items') #plot basic graph showing number of antibiotics prescribed by each practice each month over time
-plot(x='date', y='items per 1000') #plots normalised graph accounting for patient population size (prescribed items per 1,000 registered patients)
+plot(x='date', y='items', label = "Antibiotics prescribed by GP practices in Manchester CCG over time", xlabel = "Date", ylabel= "Number of antibiotics prescribed") #plot basic graph showing number of antibiotics prescribed by each practice each month over time
+plot(x='date', y='items per 1000', label = "Graph of antibiotics prescribed per 1000 patients by GP practices in Manchester CCG over time", xlabel = "Date", ylabel = "Number of antibiotics prescribed") #plots normalised graph accounting for patient population size (prescribed items per 1,000 registered patients)
 
 
 #Graph of mean for whole Manchester CCG-----------------------------------------------------------------------------------------------------------------------------------------
 prescribe_desc = new_df.groupby(by='date')['items per 1000'].describe() #Calculates mean and standard deviation across all practices for each month per 1000 registered patients
-print(prescribe_desc)
+print(prescribe_desc) #print to check df is correct and mean/std have been calculated correctly
 
 plt.subplots(figsize=(20,10)) #specifies size of plot
+plt.title(label='Mean antibiotics prescribed per 1000 patients in Manchester CCG', loc='center', pad=None)
+plt.xlabel("Date")
+plt.ylabel("Antibiotics prescribed per 1000 patients")
 plt.show(plt.plot(prescribe_desc['mean'])) #plots the graph of the mean
 
-#Graph of mean for whole Manchester CCG with standard deviation-----------------------------------------------------------------------------------------------------------------
+#Graph of mean +- 1 standard deviation for Manchester CCG-----------------------------------------------------------------------------------------------------------------
 mean_min_std = prescribe_desc['mean'] - prescribe_desc['std'] #calculates mean - standard deviation for plotting
 mean_add_std = prescribe_desc['mean'] + prescribe_desc['std'] #calculates mean + standard deviation for plotting
 
 plt.subplots(figsize=(20,10)) #plot mean, plus and minus standard deviation, need to add key
+plt.title(label='Mean plus minus one standard deviation of antibiotics prescribed per 1000 patients in Manchester CCG', loc='center', pad=None)
+plt.xlabel("Date")
+plt.ylabel("Antibiotics prescribed per 1000 patients")
+ave_plot = plt.plot(prescribe_desc['mean']) #plot mean line
+ave_plot2 = plt.plot(mean_min_std) #plot mean - standard deviation line
+ave_plot3 = plt.plot(mean_add_std) #plot mean + standard deviation line
+plt.show() #show plot
 
-ave_plot = plt.plot(prescribe_desc['mean'])
-ave_plot2 = plt.plot(mean_min_std)
-ave_plot3 = plt.plot(mean_add_std) 
-plt.show()
 
-#COMPARED TO ALL CCGs------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-#Download the data using APIs and load into pandas dataframe---------------------------------------------------------------------------------------------------------------------
-spending = requests.get('https://openprescribing.net/api/1.0/spending_by_ccg/?code=5.1&format=json').json()#gets json file of antibiotics prescribed per CCG
-#print(json.dumps(spending, indent=4, sort_keys=True)) #print data
-spendingdf = pd.DataFrame.from_dict(spending) #Creates pandas dataframe from the dictionary format of a json file
-#print(spendingdf) #prints the spending dataframe
-
-list_size = requests.get('https://openprescribing.net/api/1.0/org_details/?format=json&keys=total_list_size&org_type=ccg').json() #gets total list size per CCG
-print(json.dumps(list_size, indent=4, sort_keys=True)) #print data
-listsizedf = pd.DataFrame.from_dict(list_size) #Creates pandas dataframe from the dictionary format of a json file
-#print(listsizedf) #prints the listsize dataframe
-
-#Merge the two pandas data frames------------------------------------------------------------------------------------------------------------------------------------------------
-new_df = pd.merge(spendingdf, listsizedf, on=['row_id','date', 'row_name']) #merges the data frames on 'row_id' and 'date'
-
-#Calculate the mean antibiotics prescribed per 1000 patients across all CCGs-----------------------------------------------------------------------------------------------------
-
-new_df['items per 1000'] = new_df['items']/new_df['total_list_size']*1000 #Adds column to dataframe of prescribed items per 1,000 registered patients as some practices are smaller than others
-
-#Calculate the antibiotics prescribed per 1000 patients across Manchester CCG----------------------------------------------------------------------------------------------------
-
-prescribe_mean = new_df.groupby(by='date')['items per 1000'].mean() #Calculates mean prescriptions for each month per 1000 registered patients 
-
-#Plot graph of Manchester CCG----------------------------------------------------------------------------------------------------------------------------------------------------
 
